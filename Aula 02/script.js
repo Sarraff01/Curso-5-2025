7412760cbb1102160cc23a63a7422c6392510beb
Vinicius Sarraff <152625137+Sarraff01@users.noreply.github.com>
2025-06-26 10:25:35 -0300

    Add files via upload

---
+++ Aula 02/script.js
@@ -0,0 +1,22 @@
+const principal = document.querySelector(".container");
+const caixaPerguntas = document.querySelector(".perguntas");
+const alternativas = document.querySelector(".alternativas");
+const resultado = document.querySelector(".resultado");
+const txtResultado = document.querySelector(".txt-resultado");
+
+const perguntas = [
+    {
+    enunciado: 'Pergunta 1',
+    alternativas: [
+        'Alternativa 1',
+        'Alternativa 2'
+    
+    ]},
+    {
+        enunciado: 'Pergunta 2',
+        alternativas: [
+            'Alternativa 1',
+            'Alternativa 2'
+        
+        ]}
+]
